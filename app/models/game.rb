8f46f8313e103622102c6ce3924c341d0498fdd0
Leandro Marcucci <leanucci@gmail.com>
2020-04-15 20:41:12 -0300

    Makes test to populate game pass

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,34 @@
 class Game
+  ROW_SIZE = 16
+  COLUMN_SIZE = 16
+
+  attr_reader :rows
+
+  def initialize
+    @rows = []
+    populate!
+    self
+  end
+
+
+
+  private
+
+  def populate!
+    return false if @rows.any?
+
+    begin
+      ROW_SIZE.times do
+        row = []
+        COLUMN_SIZE.times do
+          row << Hash.new
+        end
+        rows << row
+      end
+    rescue => e
+      puts "There was an error populating the game"
+      @rows = []
+    end
+    self
+  end
 end
