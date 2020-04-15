6e35df4f27d658d3e7eca78967ddbbde4640153b
Leandro Marcucci <leanucci@gmail.com>
2020-04-15 20:45:18 -0300

    Cleans up game initialization

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game
   ROW_SIZE = 16
   COLUMN_SIZE = 16
+  BOMBS = 40
 
   attr_reader :rows
 
@@ -10,25 +11,24 @@ class Game
     self
   end
 
-
-
   private
 
   def populate!
     return false if @rows.any?
 
-    begin
-      ROW_SIZE.times do
-        row = []
-        COLUMN_SIZE.times do
-          row << Hash.new
-        end
-        rows << row
-      end
-    rescue => e
-      puts "There was an error populating the game"
-      @rows = []
-    end
+    add_rows
+    create_cells
+
     self
+  end
+
+  def add_rows
+    ROW_SIZE.times { @rows << [] }
+  end
+
+  def create_cells
+    rows.each do |row|
+      COLUMN_SIZE.times { row << Hash.new }
+    end
   end
 end
