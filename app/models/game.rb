1f73c7dbf70a256473ab0f9e5322f5581de80ccd
Leandro Marcucci <leanucci@gmail.com>
2020-04-15 22:42:20 -0300

    Ads game initialization

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,34 +1,94 @@
 class Game
-  ROW_SIZE = 16
-  COLUMN_SIZE = 16
-  BOMBS = 40
-
   attr_reader :rows
 
-  def initialize
+  def initialize(rows_num = 8, cols_num = 8)
+    @rows_num = rows_num
+    @cols_num = cols_num
+    @bomb_count = ((rows_num * cols_num) / 6).round
     @rows = []
+
     populate!
+
     self
+  end
+
+  def content
+    @content ||= rows.map do |row|
+      row.map { |r| r[:revealed] ? r[:content] : "#" }.join(" ")
+    end
   end
 
   private
 
   def populate!
-    return false if @rows.any?
-
     add_rows
     create_cells
-
-    self
+    seed_board
   end
 
   def add_rows
-    ROW_SIZE.times { @rows << [] }
+    @rows_num.times { @rows << [] }
   end
 
   def create_cells
     rows.each do |row|
-      COLUMN_SIZE.times { row << Hash.new }
+      @cols_num.times do
+        row << Hash.new
+      end
     end
+  end
+
+  def seed_board
+    seed_bombs
+    seed_content
+  end
+
+  def seed_bombs
+    bomb_locations.each do |location|
+      rows[location.first][location.last][:bomb] = true
+    end
+  end
+
+  def seed_content
+    rows.each_with_index do |row, row_index|
+      row.each_with_index do |cell, col_index|
+        cell[:revealed] = false
+        cell[:content] = nil
+
+        if cell[:bomb]
+          cell[:content] = 'b'
+        else
+          cell[:content] = find_surrounding_bombs(row_index, col_index)
+        end
+        rows[row_index][col_index] = cell
+      end
+    end
+  end
+
+  # very very simple seed implementation
+  def bomb_locations
+
+    @bomb_locations ||= begin
+      bomb_rows = (0...@rows_num).to_a.shuffle.take(@bomb_count)
+      bomb_cols = (0...@cols_num).to_a.shuffle.take(@bomb_count)
+      bomb_rows.product(bomb_cols).shuffle.take(@bomb_count)
+    end
+  end
+
+  # this method finds the content in the 8 surrounding cells:
+  # [
+  #   [row -1, col -1], [row -1, col], [row -1, col +1],
+  #   [row,    col -1],    THE CELL,   [row,    col +1],
+  #   [row +1, col -1], [row +1, col], [row +1, col +1]
+  # ]
+  #
+  def find_surrounding_bombs(row, col)
+    possible_locations = []
+    
+    row_numbers = [row -1, row, row +1].reject { |num| num.negative? || num +1 > @rows_num }
+    col_numbers = [col -1, col, col+1].reject { |num| num.negative? || num +1 > @cols_num }
+    possible_locations = row_numbers.product(col_numbers) - [row,col]
+
+    possible_locations.intersection(bomb_locations).size.to_s
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -8,13 +8,19 @@ describe Game do
     # * status: [won, lost, inprogress]
     # * public method to reveal a cell
     subject(:game) { Game.new }
-    it "has fixed dimensions" do
-      expect(subject.rows.flatten.size).to eq(Game::ROW_SIZE * Game::COLUMN_SIZE)
+    let(:bigger_game) { Game.new(16, 16) }
+
+    it "one eight of rows are bombs" do
+      expect(subject.rows.flatten.size).to eq(64)
+      expect(bigger_game.rows.flatten.size).to eq(256)
     end
 
-    it "has bombs" do
+    it "has one sixth of its size in bombs" do
       bombs = subject.rows.flatten.select { |cell| cell[:bomb] } 
-      expect(bombs.count).to eq(Game::BOMBS)
+      expect(bombs.count).to eq(10)
+
+      bigger_game_bombs = bigger_game.rows.flatten.select { |cell| cell[:bomb] }
+      expect(bigger_game_bombs.count).to eq(42)
     end
 
     it "has a start date"
